42b195bac717778e6cf3fd9052cbe96bbd852712
JavierDeSant <j.desantiagosoto@gmail.com>
2024-08-20 01:46:22 +0200

    Add input validation for the form

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { FunctionComponent } from "react";
-import { Category } from "../types/index.ts";
+import { Category, Activity } from "../types/index.ts";
 import categories from "../data/categories.ts";
 
 interface FormProps {
@@ -9,17 +9,23 @@ interface FormProps {
  
 const Form: FunctionComponent<FormProps> = () => {
 
-    const [activity, setActivity] = useState({
+    const [activity, setActivity] = useState<Activity>({
         category: 1,
         name: '',
         calories: 0
     });
 
     const handleChange = (e: React.ChangeEvent<HTMLSelectElement> | React.ChangeEvent<HTMLInputElement>) => {
+        const isNumberField = ['category', 'calories'].includes(e.target.id);
         setActivity({
             ...activity,
-            [e.target.id]: e.target.value
+            [e.target.id]: isNumberField ? +e.target.value : e.target.value
         });
+    }
+
+    const isValidActivity = () => { 
+        const { name, calories } = activity;
+        return name.trim() !== '' && calories > 0;
     }
 
     return ( 
@@ -71,8 +77,10 @@ const Form: FunctionComponent<FormProps> = () => {
 
             <input 
                 type="submit" 
-                className="bg-gray-800 hover:bg-gray-900 w-full p-2 font-bold uppercase text-white cursor-pointer"
-                value="Guardar comida o guadar ejercicio"
+                className="bg-gray-800 hover:bg-gray-900 w-full p-2 font-bold 
+                    uppercase text-white cursor-pointer disabled:opacity-10"
+                value={activity.category === 1 ? 'Guardar Comida' : 'Guardar Ejercicio'}
+                disabled={!isValidActivity()}
             />
         </form>
      );
