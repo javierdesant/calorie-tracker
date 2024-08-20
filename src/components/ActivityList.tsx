f5f7a9ea4c607fb69ead60b8bc7fad880acf5baa
JavierDeSant <j.desantiagosoto@gmail.com>
2024-08-20 03:45:43 +0200

    Add ActivityList component

---
+++ src/components/ActivityList.tsx
@@ -0,0 +1,45 @@
+import { FunctionComponent, useMemo } from "react";
+import { Activity } from "../types";
+import categories from "../data/categories";
+
+interface ActivityListProps {
+    activities: Activity[];
+}
+ 
+const ActivityList: FunctionComponent<ActivityListProps> = ({ activities }) => {
+
+    const categoryName = useMemo(() => 
+        (category: Activity['category']) => categories.map(({ id, name }) => id === category ? name : '')
+    , [activities])
+
+    return ( 
+        <>
+            <h2 className="text-4xl font-bold text-slate-600 text-center">
+                Comida y Actividades
+            </h2>
+
+            {activities.map((activity: Activity) => (
+                <div key={activity.id} className="px-5 py-10 bg-white mt-5 flex justify-between">
+                    <div className="space-y-2 relative">
+                        <p className={`absolute -top-8 -left-8 px-10 py-2 text-white uppercase font-bold 
+                            ${activity.category === 1 ? 'bg-lime-500' : 'bg-orange-500'}`}
+                        >
+                            {categoryName(activity.category)}
+                        </p>
+                        <p className="text-2xl font-bold pt-5">{activity.name}</p>
+                        <p className="font-black text-4xl text-lime-500">
+                            {activity.calories} {''}
+                            <span>Calorías</span>
+                        </p>
+                    </div>
+
+                    <div>
+
+                    </div>
+                </div>
+            ))}
+        </>
+    );
+}
+ 
+export default ActivityList;
